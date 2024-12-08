b4c6b22af6b008c76068fe580091cbb0c2958b61
ayobamy <ayobamyahmed@gmail.com>
2024-12-08 15:54:17 +0100

    (update): rm parameters

--- python-generators-0x00/4-stream_ages.py
+++ python-generators-0x00/4-stream_ages.py
@@ -3,12 +3,14 @@
 from mysql.connector import Error
 import seed
 
-def stream_user_ages(conn):
+def stream_user_ages():
     """
     Generator that yields user ages one by one from the db
     """
+    conn = None
     cursor = None
     try:
+        conn = seed.connect_to_prodev()
         cursor = conn.cursor(dictionary=True, buffered=True)
         cursor.execute("SELECT age FROM user_data")
         
@@ -21,15 +23,17 @@ def stream_user_ages(conn):
     finally:
         if cursor:
             cursor.close()
+        if conn and conn.is_connected():
+            conn.close()
 
-def calculate_average_age(conn):
+def calculate_average_age():
     """
     Calculate average age
     """
     total_age = 0
     count = 0
     
-    for age in stream_user_ages(conn):
+    for age in stream_user_ages():
         if age is not None:
             total_age += age
             count += 1
@@ -40,18 +44,12 @@ def calculate_average_age(conn):
     return total_age / count
 
 def main():
-    conn = None
     try:
-        conn = seed.connect_to_prodev()
-        if conn:
-            average_age = calculate_average_age(conn)
-            print(f"Average age of users: {average_age:.2f}")
+        average_age = calculate_average_age()
+        print(f"Average age of users: {average_age:.2f}")
             
     except Error as e:
         print(f"Error connecting to database: {e}")
-    finally:
-        if conn and conn.is_connected():
-            conn.close()
 
 if __name__ == "__main__":
     main()
