fde49e8d18e5d82fa33f291774f97dc9b5b8ed0e
ayobamy <ayobamyahmed@gmail.com>
2024-12-15 12:51:54 +0100

    (feat): resusable query

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+class ReusableQuery:
+    """
+    A CM for executing database queries with params
+    """
+    def __init__(self, db_name='airbnb.sqlite', query=None, params=None):
+        """
+        Initialize the query execution CM
+        """
+        self.db_name = db_name
+        self.query = query
+        self.params = params
+        self.connection = None
+        self.cursor = None
+        self.results = None
+
+    def __enter__(self):
+        """
+        Open DB connection and execute the query
+        """
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
+        
+        if self.query and self.params:
+            self.cursor.execute(self.query, self.params)
+            self.results = self.cursor.fetchall()
+        
+        return self.results
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """
+        Close the DB connection
+        """
+        if self.connection:
+            self.connection.close()
+        return False
+
+# test
+def main():
+    with ReusableQuery(query="SELECT * FROM users WHERE age > ?", params=(25,)) as results:
+        if results:
+            for row in results:
+                print(row)
+
+if __name__ == "__main__":
+    main()
